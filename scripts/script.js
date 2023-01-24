18ec86ee342111059d5a548c838cdbe58cabab89
akorede <emiolasaheed1@gmail.com>
2023-01-24 13:32:46 +0100

    Implement the logic of game

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,70 @@
 let playerScore = 0;
-let computerScore = 0;
+const displayPage = document.querySelector('.display');
+const choicePage = document.querySelector('.choices');
+const playerSide = document.querySelector('.choices .player');
+const computerSide = document.querySelector('.choices .computer');
+const playerImage = document.querySelector('.player img');
+const computerImage = document.querySelector('.computer img');
+const displayImages = document.querySelectorAll('.display section');
+const declareWinnerText = document.querySelector('.decide h1');
+const decideContainer = document.querySelector('.decide');
+const playAgainBtn = document.querySelector('.decide button');
+const count = document.querySelector('.count');
+//count.textContent = getLocalStorage();
+window.addEventListener('DOMContentLoaded', ()=> {
+    playerScore = getLocalStorage();
+    count.textContent = playerScore;
+})
+displayImages.forEach(item=> {
+    item.addEventListener('click', (e)=> {
+        const id = e.currentTarget.dataset.id;
+        const computerId = getComputerChoice();
+        console.log(computerId);
+        displayPage.style.display = 'none';
+        choicePage.style.display = 'flex';
+        createElements(id, playerSide);
+        setTimeout(()=> {
+            createElements(computerId, computerSide);
+        }, 2000);
+        setTimeout(()=> {
+            let winner = playRound(id, computerId);
+            game(winner);
+            count.textContent = playerScore;
+            setLocalStorage();
+            decideContainer.classList.remove('hide-decide');
+        }, 2500);
+    })
+})
+playAgainBtn.addEventListener('click', ()=> {
+    restartGame();
+})
+function restartGame() {
+    displayPage.style.display = 'flex';
+    choicePage.style.display = 'none';
+    const articles = choicePage.querySelectorAll('.img-container');
+    articles.forEach(item => {
+        item.remove();
+    })
+    decideContainer.classList.add('hide-decide');
+}
+
+function createElements(id, parentElement) {
+    const elem = document.createElement('section');
+    const imgTag = document.createElement('img');
+    elem.classList.add('img-container', id);
+    imgTag.src = `images/icon-${id}.svg`;
+    elem.append(imgTag);
+    parentElement.prepend(elem);
+}
+
 function getComputerChoice() {
     const choice = ['Rock', 'Paper', 'Scissors'];
     const rand = Math.floor(Math.random() * choice.length);
-    return choice[rand];
+    return choice[rand].toLocaleLowerCase();
 }
 function playRound(playerSelection, computerSelection) {
     const playerChoice = playerSelection.toLowerCase();
-    const computerChoice = computerSelection.toLowerCase();
+    const computerChoice = computerSelection;
     let winner = '';
     if (playerChoice === computerChoice) {
         winner =   `tie`;
@@ -27,46 +84,31 @@ function playRound(playerSelection, computerSelection) {
     }
     return winner;
 }
-function game() {
-    choices = ['rock', 'paper', 'scissors'];
-    let playerChoice = prompt('Please choice one of rock, paper, or scissors?');
-    let computerChoice = getComputerChoice();
-    if (choices.includes(playerChoice.toLowerCase())) {
-        let result = playRound(playerChoice, computerChoice);
-        if (result === 'player') {
-            playerScore++;
-        } else if (result === 'tie') {
-            playerScore = playerScore;
-            computerScore = computerScore;
-        } else {
-            computerScore++;
-        }
-    }
-    else {
-        alert('Enter a valid option');
-        game();
-    }
-}
-function displayCurrentWinner() {
-    console.log(`PlayerScore: ${playerScore} ComputerScore: ${computerScore}`);
-}
-function decideWinner() {
-    if (playerScore > computerScore) {
-        console.log(`You won!`);
-    } else if (playerScore === computerScore) {
-        console.log('tie!');
-    }
-    else {
-        console.log('You lost!');
+function game(winner) {
+    if (winner === 'player') {
+        playerScore++;
+        declareWinnerText.textContent = `YOU WIN`;
+        playAgainBtn.classList.add('win-btn');
+        playAgainBtn.classList.remove('lose-btn');
+
+    } else if (winner === 'tie') {
+        playerScore = playerScore;
+        declareWinnerText.textContent = 'TIE';
+        playAgainBtn.classList.remove('win-btn');
+        playAgainBtn.classList.remove('lose-btn');
+    } else {
+        playerScore--;
+        declareWinnerText.textContent = 'YOU LOSE';
+        playAgainBtn.classList.remove('win-btn');
+        playAgainBtn.classList.add('lose-btn');
+
     }
 }
-for (i = 0; i < 5; i++) {
-    game();
-    displayCurrentWinner();
+function setLocalStorage() {
+    localStorage.setItem('score', JSON.stringify(playerScore));
+}
+function getLocalStorage() {
+    let score = localStorage.getItem('score') ? JSON.parse(localStorage.getItem('score')) : 0;
+    return +score;
 }
 
-decideWinner();
-
-//const playerSelection = "PAPer";
-//const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, computerSelection));
